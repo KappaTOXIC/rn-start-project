48e75d75b60f6e268e626d022422e423ea5911a8
KappaTOXIC <113472884+KappaTOXIC@users.noreply.github.com>
2024-07-11 01:58:54 +0300

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,9 +4,11 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function HomeScreen() {
   return (
+
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
